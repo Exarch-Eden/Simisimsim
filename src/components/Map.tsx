65db184adea7e3b5c61fdebc6f671e588c33ae3c
Exarch-Eden <rikysmile33@gmail.com>
2021-11-26 18:42:50 -0800

    Updated Map and Sector components
    Sectors now utilize MAP_DIMENSIONS constant to render MAP_DIMENSIONS x
    MAP_DIMENSIONS many sectors in the map (supersedes Nodes)

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,52 @@
 import React from 'react'
 
 import Node from './Node'
+import Sector from './Sector'
 import { MAP_DIMENSIONS } from '../constants/map'
 
 import '../css/Map.css'
+import '../css/Sector.css'
 
 const Map = () => {
-    const renderNodes = () => {
+    const generateNodes = () => {
+
+    }
+
+    const renderSectors = () => {
+        const numSectors = MAP_DIMENSIONS * MAP_DIMENSIONS;
+        const sectors = []
+        const sectorRows = []
+
+        // assumes MAP_DIMENSIONS constant is always even
+        // may add even check later
+
+        for (let i = 0; i < numSectors; i++) {
+            console.log('i: ', i);
+            
+            sectors.push(
+                <Sector>
+                    {i}
+                    {/* {(i + 1) % 8 === 0 ? i + 1 : null} */}
+                    {renderNodes(4)}
+                </Sector>
+            )
+        }
+
+        for (let i = 0; i < numSectors; i += MAP_DIMENSIONS) {
+            sectorRows.push(
+                <div className="SectorRow">
+                    {sectors.slice(i, i + MAP_DIMENSIONS)}
+                </div>
+            )
+        }
+
+        return sectorRows;
+    }
+
+    const renderNodes = (numNodes: number) => {
         const nodes = []
-        for (let i = 0; i < MAP_DIMENSIONS; i++) {
+
+        for (let i = 0; i < numNodes; i++) {
             nodes.push(
                 <Node key={i} />
             )
@@ -19,7 +57,8 @@ const Map = () => {
 
     return (
         <div className="Map">
-            {renderNodes()}
+            {/* {renderNodes()} */}
+            {renderSectors()}
         </div>
     )
 }
--- src/components/Sector.tsx
+++ src/components/Sector.tsx
@@ -3,13 +3,12 @@ import React, { FC } from 'react'
 import '../css/Sector.css'
 
 interface SectorProps {
-    children: React.ReactNode
+    children?: React.ReactNode
 }
 
 const Sector: FC<SectorProps> = ({ children }) => {
     return (
         <div className="Sector">
-            <p>Sector</p>
             {React.Children.toArray(children)}
         </div>
     )
