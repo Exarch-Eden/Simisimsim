5ee1809d738515839516952a199743e1e2c04dcd
Exarch-Eden <rikysmile33@gmail.com>
2021-11-28 22:10:50 -0800

    Removed redundant functions in Map.js

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -12,53 +12,6 @@ interface MapProps {
 }
 
 const Map: FC<MapProps> = ({ rest }) => {
-    const generateNodes = () => {
-
-    }
-
-    const renderSectors = () => {
-        const numSectors = MAP_DIMENSIONS * MAP_DIMENSIONS;
-        const sectors = []
-        const sectorRows = []
-
-        // assumes MAP_DIMENSIONS constant is always even
-        // may add even check later
-
-        for (let i = 0; i < numSectors; i++) {
-            console.log('i: ', i);
-            
-            sectors.push(
-                <Sector>
-                    {i}
-                    {/* {(i + 1) % 8 === 0 ? i + 1 : null} */}
-                    {renderNodes(4)}
-                </Sector>
-            )
-        }
-
-        for (let i = 0; i < numSectors; i += MAP_DIMENSIONS) {
-            sectorRows.push(
-                <div className="SectorRow">
-                    {sectors.slice(i, i + MAP_DIMENSIONS)}
-                </div>
-            )
-        }
-
-        return sectorRows;
-    }
-
-    const renderNodes = (numNodes: number) => {
-        const nodes = []
-
-        for (let i = 0; i < numNodes; i++) {
-            nodes.push(
-                <Node key={i} />
-            )
-        }
-
-        return nodes;
-    }
-
     const mesh = useRef<THREE.Mesh>(null!)
 
     return (
