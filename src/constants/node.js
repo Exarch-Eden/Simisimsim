039c4a01add407257389e0054aa6aa00f2feb61d
Exarch-Eden <rikysmile33@gmail.com>
2021-12-01 07:54:13 -0800

    Updated Node JS constants file
    Added font asset url

--- src/constants/node.js
+++ src/constants/node.js
@@ -3,10 +3,12 @@ const NODE_OUT_RADIUS = 11;
 const NODE_SEGMENTS = 32;
 /** The value to add as padding between the individual nodes */
 const NODE_SIZE_BUFFER = 5;
+const NODE_FONT_URL = './assets/fonts/helvetiker_regular.typeface.json'
 
 export {
     NODE_IN_RADIUS,
     NODE_OUT_RADIUS,
     NODE_SEGMENTS,
-    NODE_SIZE_BUFFER
+    NODE_SIZE_BUFFER,
+    NODE_FONT_URL
 }
