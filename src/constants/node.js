0d4f92d548758d5030b291811b28b943bb7c78bf
Exarch-Eden <rikysmile33@gmail.com>
2021-12-01 05:34:05 -0800

    Updated node JS constants file

--- src/constants/node.js
+++ src/constants/node.js
@@ -1,9 +1,12 @@
 const NODE_IN_RADIUS = 10;
 const NODE_OUT_RADIUS = 11;
 const NODE_SEGMENTS = 32;
+/** The value to add as padding between the individual nodes */
+const NODE_SIZE_BUFFER = 5;
 
 export {
     NODE_IN_RADIUS,
     NODE_OUT_RADIUS,
     NODE_SEGMENTS,
+    NODE_SIZE_BUFFER
 }
