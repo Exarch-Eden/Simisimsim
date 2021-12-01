efcc86ce08aa382ae3843923d7c864e81c4d19d6
Exarch-Eden <rikysmile33@gmail.com>
2021-12-01 06:22:42 -0800

    Created Node and updated Generic types constant file

--- src/types/generic.ts
+++ src/types/generic.ts
@@ -3,3 +3,5 @@ import { Vector3 } from "@react-three/fiber";
 export interface Position {
     position: Vector3
 }
+
+export type Vector3Arr = [x: number, y: number, z: number]
---
+++ src/types/node.ts
@@ -0,0 +1,5 @@
+export interface NodeData {
+    id: number,
+    x: number,
+    y: number
+}
