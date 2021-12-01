fb8132b3587e6c0a914cca23bef3f2b6bff0b6cb
Exarch-Eden <rikysmile33@gmail.com>
2021-12-01 07:56:01 -0800

    Updated Node component
    Added TextGeometry with dummy string value (WIP)

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,10 +1,11 @@
+import React, { FC, useEffect, useRef, useState } from 'react'
 import { Html } from '@react-three/drei';
 import { Vector3 } from '@react-three/fiber';
-import React, { FC, useEffect, useRef } from 'react'
 import * as THREE from 'three'
+import { Font, FontLoader } from 'three';
 
 import { OUTLINE_COLOR } from '../constants/colors';
-import { NODE_IN_RADIUS, NODE_OUT_RADIUS, NODE_SEGMENTS } from '../constants/node';
+import { NODE_FONT_URL, NODE_IN_RADIUS, NODE_OUT_RADIUS, NODE_SEGMENTS } from '../constants/node';
 
 import '../css/Node.css'
 
@@ -29,6 +30,19 @@ const Node: FC<NodeProps> = ({
 }) => {
     const mesh = useRef<THREE.Mesh>(null!)
     const ringRef = useRef<THREE.BufferGeometry>(null!)
+    const [font, setFont] = useState<Font>()
+
+    useEffect(() => {
+        const fontLoader = new FontLoader()
+
+        fontLoader.load(NODE_FONT_URL, (font) => {
+            console.log('font: \n', font);
+
+            setFont(font)
+        })
+
+    }, [])
+
 
     // refer to above interface "RingGeometryArgs" for args to pass
     const ringArgs: RingGeometryArgs = [
@@ -36,24 +50,6 @@ const Node: FC<NodeProps> = ({
         NODE_OUT_RADIUS,
         NODE_SEGMENTS
     ]
-
-    // useEffect(() => {
-    //     if (ringRef) {
-    //         // console.log('centering ring geometry');
-    //         if (id && id % 40 === 0) {
-    //             ringRef.current.computeBoundingBox()
-    //             ringRef.current.computeBoundingSphere()
-    //             console.log('box center: ', ringRef.current.boundingBox?.getCenter);
-    //             console.log('sphere center: ', ringRef.current.boundingSphere?.center);
-                
-    //             // const posArr = ringRef.current.attributes.position.array;
-
-    //             // console.log('position: ',
-    //             //     posArr[id]    
-    //             // );
-    //         }
-    //     }
-    // }, [ringRef])
 
     // retrieves the position of the current node mesh relative to the map
     useEffect(() => {
@@ -64,21 +60,34 @@ const Node: FC<NodeProps> = ({
                 console.log(`mesh coordinates: [${meshVector.x}, ${meshVector.y}]`);
             }
         }
-        
+
     }, [mesh])
 
+    // TODO: center the id text (such that it is inside the ring)
     return (
-        <mesh {...rest} ref={mesh}>
-            {/* functioning test: */}
-            {/* <circleGeometry args={[1, 32]} />
+        <group>
+            <mesh {...rest} ref={mesh}>
+                {/* functioning test: */}
+                {/* <circleGeometry args={[1, 32]} />
             <meshBasicMaterial color="orange" /> */}
-            {/* testing circle outline: */}
-            <Html>
-                <p style={{}}>{id}</p>
-            </Html>
-            <ringGeometry ref={ringRef} args={ringArgs} />
-            <meshBasicMaterial color={OUTLINE_COLOR} side={THREE.FrontSide} />
-        </mesh>
+                {/* testing circle outline: */}
+                <ringGeometry ref={ringRef} args={ringArgs} />
+                <meshBasicMaterial color={OUTLINE_COLOR} side={THREE.FrontSide} />
+            </mesh>
+            <mesh>
+                {
+                    font
+                        ? (
+                            <>
+                                <textGeometry args={['Test', { font: font, size: 8 }]} />
+                                <meshBasicMaterial color={OUTLINE_COLOR} side={THREE.FrontSide} />
+                            </>
+                        )
+                        : null
+                }
+            </mesh>
+        </group>
+
     )
 }
 
