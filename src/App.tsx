21608c4f87f75742195ad8b83148d0e567067c9c
Exarch-Eden <rikysmile33@gmail.com>
2021-12-01 05:34:57 -0800

    Implemented custom generic typing to positional variables in App JS file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React, { Suspense } from 'react';
 import * as THREE from 'three'
-import { Canvas, Vector3 } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 
 import Map from './components/Map';
-import Node from './components/Node';
+import { Position } from './types/generic';
 
 import './css/App.css';
 
 // the z value is important in scaling the size of objects inside the canvas
-const canvasCamera: {
-    position: Vector3
-} = { position: [0, 0, 400] }
+const canvasCamera: Position = { position: [0, 0, 400] }
 
-const mapPosition: {
-    position: Vector3
-} = { position: [0, 0, 0] }
+const mapPosition: Position = { position: [0, 0, 0] }
 
 function App() {
     return (
