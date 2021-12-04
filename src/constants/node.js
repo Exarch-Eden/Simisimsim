55c7447cf356800f37ae1d39bf471eee9a68d4c7
Exarch-Eden <rikysmile33@gmail.com>
2021-12-04 01:15:00 -0800

    Added node constants

--- src/constants/node.js
+++ src/constants/node.js
@@ -1,14 +1,24 @@
+// FOR RING GEOMETRY
 const NODE_IN_RADIUS = 10;
 const NODE_OUT_RADIUS = 11;
 const NODE_SEGMENTS = 32;
+
+// FOR CIRCLE GEOMETRY
+const NODE_CIRC_RADIUS = 11;
+const NODE_CIRC_SEGMENTS = 32;
+
 /** The value to add as padding between the individual nodes */
 const NODE_SIZE_BUFFER = 5;
+
+// FOR FONT LOADER
 const NODE_FONT_URL = './assets/fonts/helvetiker_regular.typeface.json'
 
 export {
     NODE_IN_RADIUS,
     NODE_OUT_RADIUS,
     NODE_SEGMENTS,
+    NODE_CIRC_RADIUS,
+    NODE_CIRC_SEGMENTS,
     NODE_SIZE_BUFFER,
     NODE_FONT_URL
 }
