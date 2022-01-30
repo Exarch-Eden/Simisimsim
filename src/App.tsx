d00a76469a939f3e19a85dbec27627f1f40743bc
Exarch-Eden <rikysmile33@gmail.com>
2022-01-30 00:23:04 -0800

    Attempt at setting HUD to be positioned relative to the camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useRef } from 'react';
+import React, { Suspense, useRef, useState } from 'react';
 import { Provider } from 'react-redux';
 import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber';
@@ -23,26 +23,65 @@ const mapPosition: Position = { position: [0, 0, 1] }
 const App = () => {
     const nodeOnHover = useAppSelector(selectNodeHUDOnHover)
 
+    const appRef = useRef()
     const camRef = useRef<THREE.PerspectiveCamera>(null!)
+    const [hoverRectX, setHoverRectX] = useState(0)
+    const [hoverRectY, setHoverRectY] = useState(0)
+    const [rectWidth, setRectWidth] = useState(100)
+    const [rectHeight, setRectHeight] = useState(100)
 
     const getVisibleHeight = () => {
         const cameraOffset = camRef.current.position.z
-        
-        const vFOV = camRef.current.fov * Math.PI / 180
 
-        return 2 * Math.tan(vFOV / 2) * Math.abs(cameraOffset)
+        // const vFOV = 1 / ((cameraOffset / 400) ** 2)
+        const vFOV = window.screen.height / (1 / ((camRef.current.position.z / 400) ** 2))
+        // console.log('zoomfromz: ', vFOV);
+        
+        // const vFOV = camRef.current.fov * Math.PI / 180
+
+        return vFOV
+        // return 2 * Math.tan(vFOV / 2) * Math.abs(cameraOffset)
     }
 
     const getVisibleWidth = () => {
-        const height = getVisibleHeight()
-        return height * camRef.current.aspect
+        return window.screen.width / (1 / ((camRef.current.position.z / 400) ** 2))
+        // const height = getVisibleHeight()
+        // return height * camRef.current.aspect
     }
 
     const zoomChange = () => {
+        // console.log('z: ', camRef.current.position.z);
+        const vFOV = 1 / ((camRef.current.position.z / 400) ** 2)
+        console.log('zoomfromz: ', vFOV);
+        
+        // console.log('z^2: ', 1 / (camRef.current.position.z * camRef.current.position.z));
+        
         // logs the camera's x, y, and z when zooming in or out via OrbitControls
         console.log('cam position: ', camRef && camRef.current ? camRef.current.position : undefined);
         console.log('visibleHeight: ', getVisibleHeight());
         console.log('visibleWidth: ', getVisibleWidth());
+
+        const rectHeight = getVisibleHeight() * 0.3
+        const rectWidth = getVisibleWidth() * 0.3
+
+        const rectX = (getVisibleWidth() / 2)
+        const rectY = (getVisibleHeight() / 2)
+        // const rectX = camRef.current.position.x + (getVisibleWidth() / 2)
+        // const rectY = camRef.current.position.y - (getVisibleHeight() / 2)
+
+        console.log('rect x: ', rectX);
+        console.log('rect y: ', rectY);
+
+        // relative positioning is still a bit wonky
+        setHoverRectX(rectX)
+        setHoverRectY(rectY)
+        // setHoverRectX(camRef.current.position.x + (getVisibleWidth() / 2) - 50 - 10)
+        // setHoverRectY(camRef.current.position.y - (getVisibleHeight() / 2) + 50 + 10)
+
+        // TODO: also set the height and width of the rectangle itself to scale with the camera FOV
+        // can probably use percentage values to determine sizing
+        setRectHeight(rectHeight)
+        setRectWidth(rectWidth)
     }
 
     return (
@@ -57,7 +96,7 @@ const App = () => {
                             aspect={1920 / 1080}
                             fov={45}
                             position={[0, 0, 400]}
-                            zoom={0.5}
+                            zoom={1}
                             onUpdate={self => self.updateProjectionMatrix()}
                         />
                         <OrbitControls
@@ -84,9 +123,25 @@ const App = () => {
                                 }
                             </Html> */}
                         {
-                            nodeOnHover
+                            nodeOnHover && camRef && camRef.current
                                 ?
-                                <GenericRect rest={{ position: [0, 0, 0] }} />
+                                <>
+                                    <mesh position={[hoverRectX,hoverRectY, 2]}>
+                                        <planeGeometry args={[5, 5]} />
+                                        <meshBasicMaterial color='red' />
+                                    </mesh>
+                                    <GenericRect
+                                        width={rectWidth}
+                                        height={rectHeight}
+                                        rest={{
+                                            position: [
+                                                hoverRectX,
+                                                hoverRectY,
+                                                0
+                                            ]
+                                            // position: [MAP_WIDTH - 105, 105 - MAP_HEIGHT, 0]
+                                        }} />
+                                </>
                                 : null
                         }
                         {/* </group> */}
--- src/components/GenericRect.tsx
+++ src/components/GenericRect.tsx
@@ -1,19 +1,28 @@
-import React, { FC, useRef } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import * as THREE from 'three'
 import { OUTLINE_COLOR } from '../constants/colors'
 
 import { MeshProps } from '../types/generic'
 
 interface GenericRectProps extends MeshProps {
-
+    width?: number,
+    height?: number
 }
 
-const GenericRect: FC<GenericRectProps> = ({ rest }) => {
+const GenericRect: FC<GenericRectProps> = ({ width, height, rest }) => {
     const meshRef = useRef<THREE.Mesh>(null!)
+
+    useEffect(() => {
+        // if (meshRef && meshRef.current) {
+        //     console.log('updating center');
+        //     meshRef.current.geometry.center()
+        //     meshRef.current.updateMatrixWorld()
+        // }
+    })
 
     return (
         <mesh {...rest} ref={meshRef}>
-            <planeGeometry args={[100, 100]} />
+            <planeGeometry args={[width || 100, height || 100]} />
             <meshBasicMaterial color='white' side={THREE.FrontSide} />
         </mesh>
     )
