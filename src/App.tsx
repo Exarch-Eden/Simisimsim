3c61c708261efd102f223764580a5689cec593e4
Exarch-Eden <rikysmile33@gmail.com>
2022-01-30 18:06:02 -0800

    Updated App TSX file
    Added prop for update function callback in Canvas component
    
    Updated useEffect for canvas draw updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,56 @@
 import React, { FC, useEffect, useRef } from 'react';
 import CanvasObject from './data-classes/CanvasObject';
+import Vector3 from './data-classes/Vector3';
 
 type NativeCanvasProps = React.DetailedHTMLProps<React.CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement>
 
 interface CanvasProps {
     items: CanvasObject[],
+    updateCallback: () => void
     rest?: NativeCanvasProps
 }
 
-const Canvas: FC<CanvasProps> = ({ items, rest }) => {
+const Canvas: FC<CanvasProps> = ({ items, updateCallback, rest }) => {
     const canvasRef = useRef<HTMLCanvasElement>(null!)
-    
+
     useEffect(() => {
-        const context = canvasRef.current.getContext('webgl')
+        console.log('canvasRef.current is defined: ', canvasRef.current !== undefined);
 
+        const context = canvasRef?.current?.getContext('2d')
 
+        const interval = setInterval(() => {
+            console.log('context: ', context !== undefined);
+            if (context) {
+                updateCallback()
+                context?.clearRect(0, 0, context.canvas.width, context.canvas.height)
 
+                for (let item of items) {
+                    item.draw(context)
+                }
+            }
+        })
+
+        return () => clearInterval(interval)
     }, [])
 
     return <canvas ref={canvasRef} {...rest}></canvas>;
 };
 
 const App = () => {
+    const items: CanvasObject[] = [
+        new CanvasObject(new Vector3(0, 0)),
+        new CanvasObject(new Vector3(100, 100))
+    ]
+
+    const updateCallback = () => {
+        console.log('updateCallback()');
+        
+        
+    }
+
     return (
         <div className='App'>
-            <Canvas items={[]} rest={{ height: 2000, width: 2000 }} />
+            <Canvas items={items} updateCallback={updateCallback} rest={{ height: 2000, width: 2000 }} />
         </div>
     );
 };
