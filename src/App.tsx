a6180b35514625666a4f23bf17d0ed579d21efd5
Exarch-Eden <rikysmile33@gmail.com>
2022-03-12 02:28:38 -0800

    Updated App TSX file
    Updated updateCallback function in App FC (data items now have basic movement)
    
    More dynamic canvas width and height
    
    Added pause/unpause button
    
    Moved data (testPlanets and items) to outside of FC (prevents them from being re-rendered when pausing/unpausing the game)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import CanvasObject from './data-classes/CanvasObject';
 import Node from './data-classes/Node';
 import Planet from './data-classes/Planet';
 import Vector3 from './data-classes/Vector3';
 
+const CANVAS_HEIGHT = window.innerHeight
+const CANVAS_WIDTH = window.innerWidth
+
+const testPlanets: Planet[] = [
+    new Planet('Medium', 'Test Planet 1', 'Terra'),
+    new Planet('Medium', 'Test Planet 1', 'Arctic'),
+]
+
+const items: CanvasObject[] = [
+    new CanvasObject(new Vector3(0, 0)),
+    new CanvasObject(new Vector3(100, 100)),
+    new Node(new Vector3(200, 200), testPlanets)
+]
+
+// const CANVAS_HEIGHT = 2000
+// const CANVAS_WIDTH = 2000
+
 type NativeCanvasProps = React.DetailedHTMLProps<React.CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement>
 
 interface CanvasProps {
     items: CanvasObject[],
-    updateCallback: () => void
+    updateCallback: () => void,
+    isPaused?: boolean,
     rest?: NativeCanvasProps
 }
 
-const Canvas: FC<CanvasProps> = ({ items, updateCallback, rest }) => {
+const Canvas: FC<CanvasProps> = ({ items, updateCallback, isPaused, rest }) => {
     const canvasRef = useRef<HTMLCanvasElement>(null!)
 
     useEffect(() => {
@@ -21,9 +39,12 @@ const Canvas: FC<CanvasProps> = ({ items, updateCallback, rest }) => {
         const context = canvasRef?.current?.getContext('2d')
 
         const interval = setInterval(() => {
+            if (isPaused) return;
+
             // console.log('context: ', context !== undefined);
             if (context) {
                 updateCallback()
+                // clear the canvas in every frame before drawing
                 context?.clearRect(0, 0, context.canvas.width, context.canvas.height)
 
                 for (let item of items) {
@@ -33,32 +54,37 @@ const Canvas: FC<CanvasProps> = ({ items, updateCallback, rest }) => {
         })
 
         return () => clearInterval(interval)
-    }, [])
+    }, [isPaused])
 
     return <canvas ref={canvasRef} {...rest}></canvas>;
 };
 
 const App = () => {
-    const testPlanets: Planet[] = [
-        new Planet('Medium', 'Test Planet 1', 'Terra'),
-        new Planet('Medium', 'Test Planet 1', 'Arctic'),
-    ]
-
-    const items: CanvasObject[] = [
-        new CanvasObject(new Vector3(0, 0)),
-        new CanvasObject(new Vector3(100, 100)),
-        new Node(new Vector3(200, 200), testPlanets)
-    ]
+    const [isPaused, setIsPaused] = useState(false)
 
     const updateCallback = () => {
         // console.log('updateCallback()');
-        
-        
+
+        for (let i = 0; i < items.length; i++) {
+            const curItem = items[i]
+            curItem.move(new Vector3(2, 2))
+
+            // if canvas objects have gone past canvas
+            // reset their position to 0, 0
+            if (
+                Math.abs(curItem.position.x) > CANVAS_WIDTH ||
+                Math.abs(curItem.position.y) > CANVAS_HEIGHT
+            ) {
+                curItem.position.x = 0
+                curItem.position.y = 0
+            }
+        }
     }
 
     return (
         <div className='App'>
-            <Canvas items={items} updateCallback={updateCallback} rest={{ height: 2000, width: 2000 }} />
+            <button style={{ position: 'absolute' }} onClick={() => setIsPaused(!isPaused)}>Pause/Unpause</button>
+            <Canvas items={items} updateCallback={updateCallback} isPaused={isPaused} rest={{ height: CANVAS_HEIGHT, width: CANVAS_WIDTH }} />
         </div>
     );
 };
